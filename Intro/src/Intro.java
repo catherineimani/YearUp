9ded27c9c0247e4bacb5fe821bb819af53e1ebe5
catherineimani <ccosby@atl.yearup.org>
2015-04-30 16:21:09 -0400

    hello

---
+++ Intro/src/Intro.java
@@ -0,0 +1,9 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.print("Hello! My name is Catherine Imani. Welcome to the Java Class!");
+//I am very excited for this class. 
+	}
+
+}
